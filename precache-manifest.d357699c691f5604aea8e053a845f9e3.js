aa34f316d87a409ca0e695e518f415216f7c7fcd
Oleg <60295799+OlEzhA0@users.noreply.github.com>
2020-07-20 13:06:31 +0300

    Updates

---
+++ precache-manifest.d357699c691f5604aea8e053a845f9e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca15254bb5ae752e9c053d88cd0db1e9",
+    "url": "/mrskai-opt-drop/index.html"
+  },
+  {
+    "revision": "4dc6e59c7c5faf04f5b9",
+    "url": "/mrskai-opt-drop/static/css/main.ad784f64.chunk.css"
+  },
+  {
+    "revision": "ce69872769e98ec51d3e",
+    "url": "/mrskai-opt-drop/static/js/2.b6085473.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrskai-opt-drop/static/js/2.b6085473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc6e59c7c5faf04f5b9",
+    "url": "/mrskai-opt-drop/static/js/main.c52307c8.chunk.js"
+  },
+  {
+    "revision": "5439d85642c5679166ae",
+    "url": "/mrskai-opt-drop/static/js/runtime-main.46c09077.js"
+  },
+  {
+    "revision": "5390828f32f017cbe4ee17931d7cbc6d",
+    "url": "/mrskai-opt-drop/static/media/CeraPro-Bold.5390828f.ttf"
+  },
+  {
+    "revision": "f582d3a21e59803eb6af6411dd730f32",
+    "url": "/mrskai-opt-drop/static/media/CeraPro-Regular.f582d3a2.ttf"
+  },
+  {
+    "revision": "192c9663e9baeec2807e1e070c3a8b03",
+    "url": "/mrskai-opt-drop/static/media/CeraPro-Thin.192c9663.ttf"
+  }
+]);
